45f2348a36ce278cc652b1d04f2944be345e432c
potatonot <faddistpej.gg@gmail.com>
2026-03-10 20:44:38 +0530

    fix refresh 404 with hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './hooks/useAuth';
 import Navbar from './components/Navbar';
 import AuthPage from './pages/AuthPage';
